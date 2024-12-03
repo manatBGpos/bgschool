7a9c2a89c8b8b3ae051d39d74ca0c9f09c25c6cb
manat_laoop <manat850112@gmail.com>
2024-12-03 11:58:18 +0700

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-green-900 h-dvh flex items-center justify-center">
+    <div class="flex text-red-500 justify-center item-center">test</div>
+    <div class="flex text-red-500 justify-center item-center">test</div>
+  </div>
+</template>
+<style scoped></style>
+<script lang="ts">
+export default {
+  name: "app",
+};
+</script>
